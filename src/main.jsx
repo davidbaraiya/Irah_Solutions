8de4c8e3a1d21ac4dbb5c5e26fdd6faffc72415f
davidbaraiya <dineshbaraiya18@gmail.com>
2023-08-03 14:18:42 +0530

    modify routing

--- src/main.jsx
+++ src/main.jsx
@@ -1,15 +1,16 @@
 import React from "react";
 import { BrowserRouter } from "react-router-dom";
-import ReactDOM from "react-dom/client";
+import ReactDOM from "react-dom";
 import App from "./App.jsx";
 import "./index.css";
 import { ThemeProvider } from "@emotion/react";
 import theme from "./styles/theme";
 
-ReactDOM.createRoot(document.getElementById("root")).render(
-  <BrowserRouter>
-    <ThemeProvider theme={theme}>
+ReactDOM.render(
+  <ThemeProvider theme={theme}>
+    <BrowserRouter>
       <App />
-    </ThemeProvider>
-  </BrowserRouter>
+    </BrowserRouter>
+  </ThemeProvider>,
+  document.getElementById("root")
 );
